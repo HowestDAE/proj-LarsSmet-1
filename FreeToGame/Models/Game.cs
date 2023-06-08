89fd8579e5c1eeaf5285cd13315f10a66d908aef
SmetLars <lars.smet2002@gmail.com>
2023-06-08 19:42:14 +0200

    Added overviewpage and updated model
    Created the overviewpage for the game and updated the model by now using the correct thumbnail and game url by adding the id and name

--- FreeToGame/Models/Game.cs
+++ FreeToGame/Models/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FreeToGame.Models
 {
@@ -10,12 +11,27 @@ namespace FreeToGame.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Thumbnail { get; set; }
+
         public string Short_Description { get; set; }
-        public string Game_Url { get; set; }
+
         public string Genre { get; set; }
         public string Platform { get; set; }
-        public string Publisher { get; set; }
+
+        public string Thumbnail
+        {
+            get
+            {
+                return $"https://www.freetogame.com/g/{Id}/thumbnail.jpg";
+            }
+        }
+
+        public string Game_Url
+        {
+            get
+            {
+                return $"https://www.freetogame.com/open/{Title}";
+            }
+        }
 
     }
 }
---
+++ FreeToGame/ViewModel/OverViewPageVM.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FreeToGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeToGame.ViewModel
+{
+    public class OverViewPageVM : ObservableObject
+    {
+
+        private Game _currentGame = new Game()
+        {
+            Id = 540,
+            Title = "Overwatch",
+            Short_Description = "A hero-focused first-person team shooter from Blizzard Entertainment.",
+            Genre = "Shooter",
+            Platform = "PC (Windows)"
+
+        };
+
+        public Game CurrentGame
+        {
+            get => _currentGame;
+            set
+            {
+                _currentGame = value;
+                OnPropertyChanged(nameof(CurrentGame));
+            }
+        }
+
+
+    }
+}
